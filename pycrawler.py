1a806578c8d7a1bd682496dd93d051d6ee08f25d
Maxwell <msf9542@g.rit.edu>
2020-09-26 14:18:48 -0400

    added domain checking

--- pycrawler.py
+++ pycrawler.py
@@ -1,7 +1,8 @@
 from argparse import ArgumentParser
 import re
 
-VALID_URL = re.compile('_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS')
+VALID_URL = re.compile(
+    '((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*')
 
 
 class crawler:
@@ -30,4 +31,9 @@ class crawler:
         return parser.parse_args()
 
     def start_crawler(self):
-        self.crawler_info['domain'] = self.parser.domain
+        try:
+            self.crawler_info['domain'] = VALID_URL.match(
+                self.parser.domain).string
+        except:
+            temp = self.crawler_info['domain']
+            raise TypeError(f"given domain is not valid '" + temp + "'")
