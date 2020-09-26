d78d2a8a184db19abf14cd4e6309ea2dc3fc7787
Maxwell <msf9542@g.rit.edu>
2020-09-26 13:35:33 -0400

    added re to find check urls

--- pycrawler.py
+++ pycrawler.py
@@ -1,4 +1,7 @@
 from argparse import ArgumentParser
+import re
+
+VALID_URL = re.compile('_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS')
 
 
 class crawler:
@@ -12,7 +15,8 @@ class crawler:
         :params: n/a
         :return: n/a
         """
-        pass
+        self.parser = self.arguments()
+        self.crawler_info = {}
 
     def arguments(self):
         """
@@ -21,6 +25,9 @@ class crawler:
         :return: argparse.Namespace with parsed arguments
         """
         parser = ArgumentParser()
-        parser.add_argument(nargs=1, type=str,
+        parser.add_argument('domain', nargs=1, type=str,
                             help='target domain for webcrawler')
         return parser.parse_args()
+
+    def start_crawler(self):
+        self.crawler_info['domain'] = self.parser.domain
