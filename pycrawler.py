96f3c4b794fffec9a866eaf0795fc093bba4fc5c
Maxwell <msf9542@g.rit.edu>
2020-09-26 21:16:47 -0400

    Documentation

--- pycrawler.py
+++ pycrawler.py
@@ -12,8 +12,6 @@ class crawler:
     def __init__(self):
         """
         Initializes crawler class
-        :params: n/a
-        :return: n/a
         """
         self.parser = self.arguments()
         self.domain = ''
@@ -23,8 +21,9 @@ class crawler:
     def arguments(self):
         """
         Parses command line arguments
-        :params: n/a uses command line args
-        :return: argparse.Namespace with parsed arguments
+
+        Returns:
+            argparse.Namespace : returns parsed command line arguments
         """
         parser = ArgumentParser()
         parser.add_argument('domain', nargs=1, type=str,
@@ -32,6 +31,12 @@ class crawler:
         return parser.parse_args()
 
     def start_crawler(self):
+        """
+        Starts recursive crawler call to map a domain
+
+        Raises:
+            TypeError: raises error if domain is not valid
+        """
         if(is_valid_url(self.domain[0])):
             self.domain = self.parser.domain[0]
             print('[+] Domain -> ' + self.domain)
@@ -40,6 +45,12 @@ class crawler:
                 f'given domain is not valid "{self.parser.domain[0]}"')
 
     def crawler(self, url):
+        """
+        Recursive call for crawler that iterates through all url present with a domain
+
+        Args:
+            url ([str]): given url of the page that will be used to find all urls on the page and runs the same command
+        """
         if (self.domain not in url):
             pass
         else:
@@ -51,11 +62,22 @@ class crawler:
 
     @staticmethod
     def is_valid_url(url):
+        """
+        Checks if a url is valid 
+
+        Args:
+            url (str): url whose validity is in question
+
+        Returns:
+            bool: whether the url is valid
+        """
         parsed = urlparse(url)
         return parsed.scheme and parsed.netloc
 
     def find_urls(self, url):
-        pass
+        urls = set()
+        domain_name = urlparse(url).netloc
+        soup = BeautifulSoup(requests.get(url).content, 'html.parser')
 
 
 crawler()
